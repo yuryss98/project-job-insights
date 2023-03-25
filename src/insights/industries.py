a6d2a61e04b61622c88a33bef1c64b354ae9cc6c
Yury Stolarz <yurysscareer@gmail.com>
2023-03-25 14:09:07 -0300

    feat: add functions get_unique_industries and filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,18 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    jobs = read(path)
 
-    Must call `read`
+    list_jobs_industry = list(
+        set(job["industry"] for job in jobs if job["industry"] != "")
+    )
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return list_jobs_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    list_jobs_industry = [job for job in jobs if job["industry"] == industry]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return list_jobs_industry
