c4fd0772b0c5b13590aaf397c2bfc00d54a1b645
Yury Stolarz <yurysscareer@gmail.com>
2023-03-27 19:25:47 -0300

    test: add test for function read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,20 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+import pytest
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from unittest.mock import patch
 
 
-def test_brazilian_jobs():
-    pass
+@pytest.fixture
+def mocked_job():
+    return [{"title": "Maquinista", "salary": "2000", "type": "trainee"}]
+
+
+def mocked_read(path):
+    return [{"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}]
+
+
+def test_brazilian_jobs(mocked_job):
+    with patch("src.insights.jobs.read", mocked_read):
+        assert (
+            read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+            == mocked_job
+        )
