682a8d52f7ba7aca9d7d8298e3bea4c1380cb8d9
Yury Stolarz <yurysscareer@gmail.com>
2023-03-27 20:06:23 -0300

    test: add test for function sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,48 @@
-# from src.pre_built.sorting import sort_by
+import pytest
+from src.pre_built.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def fake_data():
+    return [
+        {
+            "title": "Front end developer",
+            "min_salary": "2000",
+            "max_salary": "3000",
+            "date_posted": "2020-10-25",
+            "type": "full time"
+        },
+        {
+            "title": "Back end developer",
+            "min_salary": "3000",
+            "max_salary": "4000",
+            "date_posted": "2021-10-25",
+            "type": "full time"
+        }
+    ]
+
+
+@pytest.fixture
+def ordem_decrescente():
+    return [
+        {
+            "title": "Back end developer",
+            "min_salary": "3000",
+            "max_salary": "4000",
+            "date_posted": "2021-10-25",
+            "type": "full time"
+        },
+        {
+            "title": "Front end developer",
+            "min_salary": "2000",
+            "max_salary": "3000",
+            "date_posted": "2020-10-25",
+            "type": "full time"
+        },
+    ]
+
+
+def test_sort_by_criteria(fake_data, ordem_decrescente):
+    sort_by(fake_data, 'max_salary')
+
+    assert fake_data == ordem_decrescente
