f90e330bdc86a4ca36e598771f6eb3613a1ec6cd
Yury Stolarz <yurysscareer@gmail.com>
2023-03-27 18:32:32 -0300

    test: add test for function count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+
+PATH = 'data/jobs.csv'
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def fake_data():
+    return """
+JavaScript JavaScript JavaScript JavaScript
+Python Python Python
+"""
+
+
+def test_counter(fake_data):
+    with patch("builtins.open", mock_open(read_data=fake_data)):
+        assert count_ocurrences(PATH, "JAVASCRIPT") == 4
